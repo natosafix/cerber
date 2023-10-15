c754b6e20e9c7393ea118cb45dc3843aade8e1dc
natosafix <92174064+natosafix@users.noreply.github.com>
2023-10-15 17:36:34 +0500

    Add persistence

---
+++ Persistence/CerberDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class CerberDbContext : DbContext
+{
+    public CerberDbContext(DbContextOptions<CerberDbContext> options) : base(options) { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<CerberDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DbConnection"));
+        });
+
+        return services;
+    }
+}
