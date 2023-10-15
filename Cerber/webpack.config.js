e2043a10fdbef776b91ab7299965a0cf60cd36e8
bolthen <92172378+bolthen@users.noreply.github.com>
2023-10-15 18:21:49 +0500

    webpack, typescript, react base configuration

---
+++ Cerber/webpack.config.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+
+module.exports = {
+    mode: 'development',
+    entry: "./Content/entries/pages/index.tsx",
+    output: {
+        filename: "./dist/reactApp.js"
+    },
+    watch: true,
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.scss'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ]
+    }
+}
