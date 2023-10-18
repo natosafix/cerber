b0459cd9e63a1749e8c8c6d98857a9b38b3c9074
bolthen <92172378+bolthen@users.noreply.github.com>
2023-10-18 21:06:04 +0500

    Не работает scss

--- Cerber/Content/Components/header/header.tsx
+++ Cerber/Content/Components/header/header.tsx
@@ -1,8 +1,9 @@
 ﻿import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import styles from './header.module.scss';
+import styles from './header.scss';
 
 export const Header: React.FC = () => {
+    // return <div className={styles.headerWrapper}>I'm aa Header</div>;
     return <div className={'headerWrapper'}>I'm aa Header</div>;
 };
