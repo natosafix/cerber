d73ae26cf76a4bfa671d0515a55fbf7e50df8657
bolthen <92172378+bolthen@users.noreply.github.com>
2023-10-20 02:28:26 +0500

    Добавил хедер

--- Web/Content/Components/header/header.tsx
+++ Web/Content/Components/header/header.tsx
@@ -1,20 +1,15 @@
 ﻿import * as React from 'react';
-import { Group, Button, Textarea } from '@skbkontur/react-ui';
+import { Group, Button, Textarea, Link, Gapped } from '@skbkontur/react-ui';
 
 import styles from './header.scss';
 
 export const Header: React.FC = () => {
-    // return <div className={styles.headerWrapper}>I'm a Header</div>;
-    const [value, setValue] = React.useState('Значение');
-    
-    return <Group>
-        <Textarea
-            value={value}
-            onValueChange={setValue}
-            autoResize
-            rows={1}
-            placeholder="Плейсхолдер"
-        />
-        <Button style={{ height: '52px' }} onClick={() => setValue('')}>Очистить значение</Button>
-    </Group>
+    return (
+        <div className={styles.headerWrapper}>
+            <p className={styles.logo}>CERBER</p>
+            <Gapped gap={20} verticalAlign={'middle'}>
+                <Button size={'medium'} use={'text'}>Иванов Иван</Button>
+                <Button size={'medium'}>Выйти</Button>
+            </Gapped>
+        </div>);
 };
---
+++ Web/Content/entries/Shared/CerberThemeProvider.tsx
@@ -0,0 +1,12 @@
+﻿import { THEME_2022, ThemeContext, ThemeFactory } from '@skbkontur/react-ui';
+import * as React from 'react';
+import styles from './CerberThemeProvider.scss';
+
+export const CerberThemeProvider = ({children}) => {
+    return (
+        <ThemeContext.Provider value={THEME_2022}>
+            <div className={styles.pageWrapper}>
+                {children}
+            </div>
+        </ThemeContext.Provider>);
+};
