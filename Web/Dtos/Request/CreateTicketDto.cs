00756eef60f5799a19e55ed79f532f9f986c2bd6
natosafix <artemburdinnn@gmail.com>
2023-12-19 14:36:04 +0500

    Add name to CreateTicketDto

--- Web/Dtos/Request/CreateTicketDto.cs
+++ Web/Dtos/Request/CreateTicketDto.cs
@@ -5,6 +5,9 @@ namespace Web.Dtos.Request;
 public class CreateTicketDto
 {
     [Required]
+    public string Name { get; set; }
+    
+    [Required]
     public string Price { get; set; }
 
     [Required]
