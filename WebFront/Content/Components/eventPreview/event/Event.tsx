7e25f2540b4be01ee5c4f5c1118f510721b72673
Egor Loparev <egorloparev@gmail.com>
2024-01-11 23:46:23 +0500

    Fix children bag

--- WebFront/Content/Components/eventPreview/event/Event.tsx
+++ WebFront/Content/Components/eventPreview/event/Event.tsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import {IEvent} from '../models/index'
+import React, { useState } from 'react';
+import { IEvent } from '../models/index';
 import styles from './event.scss';
 
 interface IProps {
-  event: IEvent;
+    event: IEvent;
 }
 
 const Event = ({ event }: IProps) => {
-  const {
-      img,
-      title
-  } = event;
+    const { img, title } = event;
+    const [isHovered, setIsHovered] = useState(false);
 
     const handleClick = () => {
-        window.location.href = 'https://www.google.com'; // Замените на ваш URL
+        window.location.href = 'https://www.google.com';
     };
 
-  return (
-        <div className={styles.eventWrapper}>
-12
+    return (
+        <div
+            className={styles.eventWrapper}
+            onClick={handleClick}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            style={{ cursor: isHovered ? 'pointer' : 'default' }}
+        >
             <div className={styles.imgWrapper}>
-                <img  src="https://content-30.foto.my.mail.ru/community/bibliadenjzadnjom/1/h-36648.jpg"/>
+                <img src="https://content-30.foto.my.mail.ru/community/bibliadenjzadnjom/1/h-36648.jpg" />
             </div>
             <div className={styles.textWrapper}>
                 <p>{title}</p>
             </div>
         </div>
-
-  );
+    );
 };
 
-export default Event
+export default Event;
