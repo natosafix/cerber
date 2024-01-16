29b747d5c127334d32d9bd5d63f1e75e6d7a947b
andrei-uni <92112053+andrei-uni@users.noreply.github.com>
2024-01-16 05:08:03 +0500

    add internet permission

--- mobile/lib/utils/network_checker/network_checker_impl.dart
+++ mobile/lib/utils/network_checker/network_checker_impl.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
+import 'package:project/utils/constants/constants.dart';
 import 'package:project/utils/network_checker/network_checker.dart';
 
 class NetworkCheckerImpl implements NetworkChecker {
   @override
   Future<bool> networkAvailable() async {
     try {
-      final result = await InternetAddress.lookup('example.com');
+      final result = await InternetAddress.lookup(Constants.eventsRepositoryBaseUrl);
       return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
     } on SocketException catch (_) {
       return false;
