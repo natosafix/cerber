df79872a67024e73d323f622016fb3625cd932df
natosafix <artemburdinnn@gmail.com>
2024-01-16 18:49:30 +0500

    Fix toAttachment

--- Domain/Infrastructure/Extenstions/ImageInfoExtensions.cs
+++ Domain/Infrastructure/Extenstions/ImageInfoExtensions.cs
@@ -6,9 +6,10 @@ public static class ImageInfoExtensions
 {
     public static MimePart ToAttachment(this ImageInfo imageInfo)
     {
+        using var memoryStream = new MemoryStream(imageInfo.Content);
         return new MimePart("image", "png")
         {
-            Content = new MimeContent(new MemoryStream(imageInfo.Content)),
+            Content = new MimeContent(memoryStream),
             ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
             ContentTransferEncoding = ContentEncoding.Base64,
             FileName = imageInfo.FileName
