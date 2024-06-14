7cf80ce76c10df441ca304e4812268bb2460a9d0
Egor Loparev <egorloparev@gmail.com>
2024-06-14 23:31:00 +0500

    fix footer

--- WebFront/Content/Entries/Shared/Footer/Footer.tsx
+++ WebFront/Content/Entries/Shared/Footer/Footer.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
-import { Button, Gapped } from '@skbkontur/react-ui';
 import styles from './Footer.scss';
 import { MaxWidthWrapper } from '../Wrappers/MaxWidthWrapper';
 import file from '../../../../Files/Public-Oferta.docx';
 
 export const Footer: React.FC = () => {
     return (
-        <footer className={styles.headerWrapper}>
+        <footer className={styles.footerWrapper}>
             <MaxWidthWrapper>
-                <div className={styles.headerSpaceBetweenWrapper}>
-                    <Gapped gap={20} verticalAlign={'middle'}>
-                        <a href={file}>{'Публичная оферта'}</a>
-                    </Gapped>
-                </div>
+                <a href={file}>{'Публичная оферта'}</a>
             </MaxWidthWrapper>
         </footer>
     );
